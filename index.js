6650713dc1e58d0df89ec7d9c4fb93cfbcca208b
Quinn Lawson <sunsfan1998@cox.net>
2019-03-27 22:13:38 -0700

    main.js now connects to the web server and prints hello world

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.send("Hello World!");
 })
 
