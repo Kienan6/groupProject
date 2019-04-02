56a2270acc5adbf3069e9fe68ae882cb7a3ffa8c
Quinn Lawson <sunsfan1998@cox.net>
2019-04-01 20:17:08 -0700

    Added login functionality

--- index.js
+++ index.js
@@ -1,17 +1,74 @@
 const express = require('express')
-const app = express()
+const app = express();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "csc-346-group-project.cf7ec6pc0dbo.us-east-1.rds.amazonaws.com",
+  user: "quinnal2",
+  password: "csc346proj"
+});
 
-//app.use(express.static('public'));
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected");
+});
+con.query("USE csc_346_group_project", function (err, result) {
+  if (err) throw err;
+});
 
-app.get('/', (req, res) => {
-  res.send('HEY!')
-})
-app.locals.title = "new app"
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.use(express.static('public'));
 
-/*app.get('/', function (req, res) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.send("Hello World!");
+  res.header("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+app.get('/', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  if(req.query.mode == "singleUser") {
+    let sql = "SELECT id FROM users WHERE userName = '" + req.query.userName + "'";
+    con.query(sql, function (err, result, fields) {
+      if (err) throw err;
+      if(result.length > 0) {
+        sql = "SELECT id FROM users WHERE password = '" + req.query.password + "'";
+        con.query(sql, function(err2, result2, fields2) {
+          if(result2.length > 0) {
+            res.send("success");
+          } else {
+            res.send("failure");
+          }
+        });
+      } else {
+        res.send("failure");
+      }
+    });
+  }
+});
+
+app.post('/', jsonParser, function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  if(req.body.mode == "newUser") {
+    let sql = "SELECT id FROM users WHERE userName = '" + req.body.userName + "'";
+    con.query(sql, function(err, result, fields) {
+      if (err) throw err;
+      console.log("Length: " + result.length);
+      if(result.length == 0) {
+        sql = "INSERT INTO users (userName, password) VALUES ('" + req.body.userName + "','" + req.body.password + "')";
+        con.query(sql, function (err2, result2) {
+          if (err2) throw err2;
+          console.log("1 record inserted");
+          res.send("success");
+        });
+      } else {
+        res.send("failure");
+      }
+    });
+  }
 })
 
-app.listen(3000); quinn's code */
+app.locals.title = "new app";
+
+app.listen(3000);
--- main.js
+++ main.js
@@ -2,15 +2,59 @@
 (function(){
   function initialize() {
     //let url = "http://ec2-3-85-40-76.compute-1.amazonaws.com";
-    let url = "http://localhost:3000"
+    document.getElementById("log-in-button").onclick = logIn;
+    document.getElementById("create-account-button").onclick = createAccount;
+  }
 
+  function logIn() {
+    let userName = prompt("Username", "");
+    let password = prompt("Password:", "");
+    let url = "http://localhost:3000?mode=singleUser&userName=" + userName + "&password=" + password;
     fetch(url)
       .then(checkStatus)
       .then(function(responseText) {
-        let helloWorld = document.createElement("p");
+        if(responseText == "success") {
+          document.getElementById("log-in").innerHTML = "";
+          let successText = document.createElement("p");
+          let logOutButton = document.createElement("button");
 
-        helloWorld.innerHTML = responseText;
-        document.getElementById("content").appendChild(helloWorld);
+          successText.innerHTML = "Logged in as " + userName;
+          logOutButton.innerHTML = "Log Out";
+          logOutButton.id = "log-out-button";
+          logOutButton.onclick = logOut();
+          document.getElementById("log-in").appendChild(successText);
+        } else {
+          alert("Failed to log in. Username or password is incorrect");
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+    });
+  }
+
+  function createAccount() {
+    let userName = prompt("Enter a username:", "");
+    let password = prompt("Enter a password:", "");
+    const message = {mode: "newUser",
+                     userName: userName,
+                     password: password}; // set fields of the message object to the current name and comment
+    const fetchOptions = {
+        method : 'POST',
+        headers : {
+            'Accept': 'application/json',
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify(message)
+    };
+    let url = "http://localhost:3000";
+    fetch(url, fetchOptions)
+      .then(checkStatus)
+      .then(function(responseText) {
+        if(responseText == "success") {
+          alert("Account successfully created");
+        } else {
+          alert("Failed to create account. Username already exists.");
+        }
       })
       .catch(function(error) {
         console.log(error);
@@ -18,7 +62,17 @@
   }
 
   function checkStatus(response) {
-    return response.text();
+    if (response.status >= 200 && response.status < 300) {
+      return response.text();
+    } else if (response.status == 404) {
+      return Promise.reject(new Error("Sorry, we couldn't find that page"));
+    } else {
+      return Promise.reject(new Error(response.status + ": " + response.statusText));
+    }
+  }
+
+  function logOut() {
+
   }
 
   window.onload = initialize;
